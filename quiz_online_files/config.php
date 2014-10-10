4bd309fd0391f9835eb93b82deb7624f8a7bf86b
ChachaSoriano <chaSoriano12@gmail.com>
2014-10-10 14:08:04 -0700

    OHMYRIDDLE
    RIDDLE

---
+++ quiz_online_files/config.php
@@ -0,0 +1,8 @@
+<?php
+	$host = "mysql.hostinger.ph";
+	$user = "u560402632_jessa";
+	$pass = "Aa1234567";
+	$db = "u560402632_jessa";
+	$conn = @mysqli_connect ($host,$user,$pass,$db) OR die ('Could not connect to
+	MySQL: ' . mysqli_connect_error( ) );
+?>
